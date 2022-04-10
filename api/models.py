0dd3fa5aa6f73e513c1c333efc08c90deb041d9b
Dennis <bouncy.lu@hotmail.com>
2022-04-10 12:14:16 +0800

    Add models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Venue(models.Model):
+    TYPES = (
+        ('LT', 'Lecture Theatre'),
+        ('CR', 'Classroom'),
+        ('TR', 'Tutorial Room'),
+    )
+    code = models.CharField(max_length=20, primary_key=True)
+    location = models.CharField(max_length=150)
+    type = models.CharField(max_length=2, choices=TYPES)
+    capacity = models.IntegerField()
+
+    def __str__(self):
+        return self.code
+
+
+class HkuMember(models.Model):
+    hku_id = models.CharField(max_length=10, primary_key=True)
+    name = models.CharField(max_length=150)
+    venues = models.ManyToManyField(Venue, through='Visit')
+
+    def __str__(self):
+        return f'{self.hku_id} {self.name}'
+
+
+class Visit(models.Model):
+    ACTIONS = (
+        ('I', 'Entry'),
+        ('O', 'Exit'),
+    )
+    time = models.DateTimeField()
+    action = models.CharField(max_length=1, choices=ACTIONS)
+    hku_member = models.ForeignKey(HkuMember, on_delete=models.CASCADE)
+    venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.time} {self.action} {self.hku_member.hku_id} {self.venue.code}'
--- core/settings.py
+++ core/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rest_framework',
-    'api.apps.ApiConfig',
+    'api',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
