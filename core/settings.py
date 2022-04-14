f60724afc269c2f941f3d6f1736f18d96ac1b477
Dennis <bouncy.lu@hotmail.com>
2022-04-14 21:36:01 +0800

    Remove BrowsableAPIRenderer

--- core/settings.py
+++ core/settings.py
@@ -55,6 +55,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
 }
 
 MIDDLEWARE = [
