73fe2bf44ba13ca54d28df80d7b5597c57a3d308
Dennis <bouncy.lu@hotmail.com>
2022-04-14 21:10:51 +0800

    Collect statics

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os.path
+import sys
+
+PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_URL = '/static/'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
